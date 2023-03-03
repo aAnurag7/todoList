f7cb0068cca6a17e973b2877faf638b212d28981
Anurag <achaturvedi@qodeleaf.com>
2023-03-03 16:24:51 +0530

    feat: add dashboard
    summary: user can use dashboard after login

---
+++ client/src/pages/Dashboard/DashboardComponents/Dropdown/Dropdown.js
@@ -0,0 +1,108 @@
+// import React, { useEffect, useRef } from "react";
+
+// function Dropdown(props){
+
+//     const dropdownRef=useRef()
+
+//     const handleClick=(event)=>{
+//         if(dropdownRef && !dropdownRef.current.contains(event.target))
+//         {
+//             if(props.onClose)props.onClose()
+//         }
+        
+//         // // eslint-disable-next-line no-unused-expressions
+//         //  props.onClose ? props.onClose():"";
+        
+//     }
+
+//     useEffect(()=>{
+//         document.addEventListener('click',handleClick)
+
+//         return()=>{
+//             document.removeEventListener('click',handleClick)
+//         }
+//     })
+
+//     return(
+//         <div ref={dropdownRef} className="dropdown" style={{
+//             position:"absolute",
+//             top:"100%",
+//             right:"0"
+//         }}>
+//             {props.children}
+//         </div>
+//     )
+// }
+
+// export default Dropdown
+// import React, { useEffect, useRef } from "react";
+
+// import "./Dropdown.css";
+
+// function Dropdown(props) {
+//   const dropdownRef = useRef();
+
+//   const handleClick = (event) => {
+//     if (
+//       dropdownRef &&
+//       !dropdownRef.current?.contains(event.target) &&
+//       props.onClose
+//     )
+//       props.onClose();
+//   };
+
+//   useEffect(() => {
+//     document.addEventListener("click", handleClick);
+
+//     return () => {
+//       document.removeEventListener("click", handleClick);
+//     };
+//   });
+
+//   return (
+//     <div
+//       ref={dropdownRef}
+//       className={`dropdown custom-scroll ${props.class ? props.class : ""}`}
+//     >
+//       {props.children}
+//     </div>
+//   );
+// }
+
+// export default Dropdown;
+
+import React, { useEffect, useRef } from "react";
+
+import "./Dropdown.css";
+
+function Dropdown(props) {
+  const dropdownRef = useRef();
+
+  const handleClick = (event) => {
+    if (
+      dropdownRef &&
+      !dropdownRef.current?.contains(event.target) &&
+      props.onClose
+    )
+      props.onClose();
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  });
+
+  return (
+    <div
+      ref={dropdownRef}
+      className={`dropdown custom-scroll ${props.class ? props.class : ""}`}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Dropdown;
