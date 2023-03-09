9f94ebf902e45c26932911142f4d7c89381e3545
Anurag <achaturvedi@qodeleaf.com>
2023-03-09 16:31:10 +0530

    feat: add put method to update database

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,68 +1,98 @@
-import React from 'react'
-import { useState } from 'react'
-import {FcGoogle} from 'react-icons/fc'
-import '../styles/login.css'
+import React from "react";
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import "../styles/login.css";
 
-const Login=()=>{
-    const [email, setEmail]=useState("")
-    const [password, setPassword]=useState("")
-    const check = async (e) =>{
-        e.preventDefault();
-        let values = {email: email, password: password};
-        values = JSON.stringify(values)
-     let res =await fetch('http://localhost:4000/login',{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials":true,
-                "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
-                "Access-Control-Max-Age": 86400,
-            },
-            body: values
-        })
-        let data = await res.json();
-        if(res.status === 200){
-            localStorage.setItem('token',JSON.stringify(data));
-             window.location = '/board'
-        }
-        else if(res.status === 404)alert(`${res.status} user not found`)
-        else if(res.status === 401)alert(`${res.status} wrong password`)
-        else{alert(`${res.status} server error`)}
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const check = async (e) => {
+    e.preventDefault();
+    let values = { email: email, password: password };
+    values = JSON.stringify(values);
+    let res = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+        "Access-Control-Max-Age": 86400,
+      },
+      body: values,
+    });
+    let data = await res.json();
+    if (res.status === 200) {
+      localStorage.setItem("token", JSON.stringify(data));
+      window.location = "/board";
+    } else if (res.status === 404) alert(`${res.status} user not found`);
+    else if (res.status === 401) alert(`${res.status} wrong password`);
+    else if (res.status === 400) alert(`${res.status} Invalid credentials`);
+    else {
+      alert(`${res.status} server error`);
     }
-  return(
+  };
+  return (
     <>
-   <div className="main">
+      <div className="main">
         <div className="navbar">
-            <div className="icon">
-                <h2 id="title"className="logo">Todo</h2>
-            </div>
-        </div> 
-       <div className="content">
-            <h1>Welcome to do  <br/><span>task</span> <br/></h1>
-            <p className="par"> Todo involves creating a tool for
-                managing tasks and increasing productivity & <br/>
-                To create a user-friendly and intuitive interface for users to
-                create, organize,<br/> and prioritize tasks in order to stay on top of their
-                workload and meet their goals.</p>
-                
-              <div className="form"> 
-                    <h2>Login Here</h2>
-                    <input type="email" name="email" value = {email} onChange={(e=>setEmail(e.target.value))} placeholder="Enter Email Here"/>
-                    <input type="password" name="" value = {password} onChange={(e=>setPassword(e.target.value))} placeholder="Enter Password Here"/>
-                    <button className="btnn" onClick={check}>Login</button>
-
-                    <p className="link">Don't have an account<br/>
-                    <a href="/Register">Sign up here</a></p>
-                    <div className="icons">
-                    <p className="liw">Log in with</p><a id="icongoogle" href="/board"><FcGoogle/></a>
-                    </div>
-                </div>
+          <div className="icon">
+            <h2 id="title" className="logo">
+              Todo
+            </h2>
+          </div>
         </div>
-    </div>
-    
-        </>
-  )
-}
+        <div className="content">
+          <h1>
+            Welcome to do <br />
+            <span>task</span> <br />
+          </h1>
+          <p className="par">
+            {" "}
+            Todo involves creating a tool for managing tasks and increasing
+            productivity & <br />
+            To create a user-friendly and intuitive interface for users to
+            create, organize,
+            <br /> and prioritize tasks in order to stay on top of their
+            workload and meet their goals.
+          </p>
 
-export default Login
+          <div className="form">
+            <h2>Login Here</h2>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email "
+            />
+            <input
+              type="password"
+              name=""
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+            <button className="btnn" onClick={check}>
+              Login
+            </button>
+
+            <p className="link">
+              Don't have an account
+              <br />
+              <a href="/Register">Sign up here</a>
+            </p>
+            <div className="icons">
+              <p className="liw">Log in with</p>
+              <a id="icongoogle" href="/google">
+                <FcGoogle />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,55 +1,74 @@
-import React from 'react'
-import { useState } from 'react'
-import '../styles/register.css'
+import React from "react";
+import { useState } from "react";
+import "../styles/register.css";
 
+const Register = () => {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const Register=()=>{
-  const [fullname, setFullname]=useState("")
-  const [email, setEmail]=useState("")
-    const [password, setPassword]=useState("")
-
-
-    const checkforregistration = async (e) =>{
-        e.preventDefault();
-        let values = {fullname:fullname,email: email, password: password};
-        values = JSON.stringify(values);
-    fetch('http://localhost:4000/create/user',{
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Credentials":true,
-              "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
-              "Access-Control-Max-Age": 86400,
-            },
-            body: values
-        }).then((res)=>{
-          console.log(res.status)
-            if(res.status === 201){
-                console.log('register succesfuly')
-                window.location = '/'
-            }
-            else{
-                console.log(res.status)
-                // window.location = '/'
-            }
-        })
+  const checkforregistration = async (e) => {
+    e.preventDefault();
+    let values = { fullname: fullname, email: email, password: password };
+    values = JSON.stringify(values);
+    fetch("http://localhost:5000/create/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+        "Access-Control-Max-Age": 86400,
+      },
+      body: values,
+    }).then((res) => {
+      console.log(res.status);
+      if (res.status === 201) {
+        console.log("register succesfuly");
+        window.location = "/";
+      } else {
+        console.log(res.status);
+        alert(`${res.status}`);
+        // window.location = '/'
       }
-  return(
+    });
+  };
+  return (
     <>
-    <div className='main1'>
-        <div className='form1'> 
-            <h2>Sign up Here</h2>
-            <input value = {fullname} onChange={(e)=>setFullname(e.target.value)} type="text" name="fullname" placeholder="Enter Name Here"/>
-            <input value = {email} onChange={(e)=>setEmail(e.target.value)} type="email" name="email" placeholder="Enter Email Here"/>
-            <input value = {password} onChange={(e)=>setPassword(e.target.value)} type="password" name="" placeholder="Enter Password Here"/>
-            <button className="btnn" onClick={checkforregistration}>Register</button>
-            <p className="link"><a href="/">back</a></p>
+      <div className="main1">
+        <div className="form1">
+          <h2>Sign up Here</h2>
+          <input
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
+            type="text"
+            name="fullname"
+            placeholder="Name "
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            name="email"
+            placeholder="Email "
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            name=""
+            placeholder="Password"
+          />
+          <button className="btnn" onClick={checkforregistration}>
+            Register
+          </button>
+          <p className="link">
+            <a href="/">back</a>
+          </p>
         </div>
-    </div>
-     
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Register;
